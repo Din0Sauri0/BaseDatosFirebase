e2cdedc04bde3950026b56447af23c13ab812059
Gustavo <gustavo.ovalle.zambrano@gmail.com>
2020-11-18 12:22:35 -0300

    Validaciones al crear el usuario. metodo para mensajes. Actualizacion y delete realizados

--- app/src/main/java/com/ovalle/basedatosfirebase/MainActivity.java
+++ app/src/main/java/com/ovalle/basedatosfirebase/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -55,20 +56,90 @@ public class MainActivity extends AppCompatActivity {
         // Conexion con firebase
         conectarFirebase();
         cargarRecycler();
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reference.child("Usuario").child(usuario.getId()).removeValue(new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                        mensajeToast("El usuario ha sido eliminado");
+                    }
+                });
+            }
+        });
+
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newNombre = txtNombre.getText().toString();
+                String newGenero = spGenero.getSelectedItem().toString();
+                usuario.setNombre(newNombre);
+                usuario.setGenero(newGenero);
+                reference.child("Usuario").child(usuario.getId()).setValue(usuario, new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                        mensajeToast("El usuario ha sido actualizado");
+                    }
+                });
+            }
+        });
+
+        recyclerUsuarios.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+                View view = rv.findChildViewUnder(e.getX(), e.getY());
+                if (view != null){
+                    int position = rv.getChildAdapterPosition(view);
+                    usuario = arrayListUsuario.get(position);
+                    txtNombre.setText(usuario.getNombre());
+                    if (usuario.getGenero().equals("Masculino")){
+                        spGenero.setSelection(1);
+                    }else if(usuario.getGenero().equals("Femenino")){
+                        spGenero.setSelection(2);
+                    }else{
+                        spGenero.setSelection(3);
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+
         btnCrear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Capturar datos de UI
-                String id = UUID.randomUUID().toString();
-                String nombre = txtNombre.getText().toString().trim();
-                String genero = spGenero.getSelectedItem().toString();
-                // Generar objeto de la clase usuario
-                Usuario usuario = new Usuario(id, nombre, genero);
-                insertarUsuario(usuario);
+                if (spGenero.getSelectedItemPosition() == 0) {
+                    mensajeToast("Seleccione un genero");
+                }else if (txtNombre.getText().toString() == null) {
+                    mensajeToast("El nombre esta vacio");
+                } else {
+                    String id = UUID.randomUUID().toString();
+                    String nombre = txtNombre.getText().toString().trim();
+                    String genero = spGenero.getSelectedItem().toString();
+                    // Generar objeto de la clase usuario
+                    Usuario usuario = new Usuario(id, nombre, genero);
+                    insertarUsuario(usuario);
+                    limpiarFormulario();
+                }
             }
         });
     }
 
+    public void mensajeToast(String mensaje){
+        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
+    }
     public void conectarFirebase(){
         database = FirebaseDatabase.getInstance();
         reference = database.getReference();
@@ -111,5 +182,10 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void limpiarFormulario(){
+        txtNombre.setText(null);
+        spGenero.setSelection(0);
     }
 }
